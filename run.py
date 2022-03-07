d1ad259e29ba2dd9ccc7800e9065e0b812f73d2c
richbruton <richbruton@gmail.com>
2022-03-07 17:52:56 +0000

    retrieve lastest stock numbers from spreadsheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -37,10 +38,12 @@ def get_sales_data():
 def validate_data(values):
     """
     Get sales figures input from the user.
-    Run a while loop  to collect a valid string of data from the user via the terminal,
-    which must be a string of 6 numbers seperated by commas. 
+    Run a while loop  to collect a valid string of data from
+     the user via the terminal,
+    which must be a string of 6 numbers seperated by commas.
     The loop will repeatedly request data, until it is valid
-    """    
+    """
+
     try:
         [int(value) for value in values]
         if len(values) != 6:
@@ -53,7 +56,9 @@ def validate_data(values):
 
     return True
 
+
 def update_sales_worksheet(data):
+
     """
     Update sales worksheet, add new row with the list data provided.
     """
@@ -62,9 +67,28 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate the surplus for each item type.
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+    The Surplus is defined as the sales figure subtraced from teh stock:
+    - Positive surplus indicates waste
+    - negative surples indicates extras made when stock runs out
+    """
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
 
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
 
+print("Welcome to Love Sandwiches data automation.")
+main()
